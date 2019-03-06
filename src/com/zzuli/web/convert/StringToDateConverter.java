f01677d3a077bb8c2598d042bc4893c15829652b
WE <WE@DESKTOP-0V8124L>
2019-03-06 12:45:05 +0800

    SpringMVC学习

---
+++ src/com/zzuli/web/convert/StringToDateConverter.java
@@ -0,0 +1,28 @@
+package com.zzuli.web.convert;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+/**
+ * 自定义一个类型转换器类
+ * @author WE
+ *
+ */
+public class StringToDateConverter implements Converter<String, Date>{
+
+	@Override
+	public Date convert(String arg0) {
+		SimpleDateFormat dateFormate = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = dateFormate.parse(arg0);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+}
